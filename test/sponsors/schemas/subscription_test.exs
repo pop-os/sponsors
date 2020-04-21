22e193a034bf0f6c6629bb547cf3231d6a5ba84a
Sean Callan <seancallan@gmail.com>
2020-04-21 10:27:50 -0600

    Add Subscription and Invoice schemas

---
+++ test/sponsors/schemas/subscription_test.exs
@@ -0,0 +1,19 @@
+defmodule Sponsors.Schemas.SubscriptionTest do
+  use Sponsors.DataCase
+
+  import Sponsors.Factory
+
+  alias Sponsors.Schemas.Subscription
+
+  describe "changeset/2" do
+    test "returns a valid changeset" do
+      params = params_for(:subscription)
+      assert %{valid?: true} = Subscription.changeset(%Subscription{}, params)
+    end
+
+    test "returns an invalid changeset when missing required fields" do
+      without_customer = params_for(:subscription, internal_customer_id: nil)
+      assert %{valid?: false} = Subscription.changeset(%Subscription{}, without_customer)
+    end
+  end
+end
---
+++ test/support/factory.exs
@@ -0,0 +1,21 @@
+defmodule Sponsors.Factory do
+  use ExMachina.Ecto, repo: Sponsors.Repo
+
+  alias Sponsors.Schemas.{Invoice, Subscription}
+
+  def invoice_factory do
+    %Invoice{
+      paid: false,
+      stripe_invoice_id: sequence(:invoice_id, &"in_1GaOY2ALgoDNBXZTJGI2sn#{&1}"),
+      subscription: build(:subscription)
+    }
+  end
+
+  def subscription_factory do
+    %Subscription{
+      canceled: false,
+      internal_customer_id: sequence(:customer_id, &"1234#{&1}")
+      stripe_subscription_id: sequence(:subscription_id, &"sub_H8gD56NJYhAf#{&1}"),
+    }
+  end
+end
