551bbc3361b4952272d7c5017f428b86d507b07b
Sean Callan <seancallan@gmail.com>
2020-04-21 08:07:30 -0600

    Remove sockets

--- lib/sponsors_web/endpoint.ex
+++ lib/sponsors_web/endpoint.ex
@@ -10,20 +10,6 @@ defmodule SponsorsWeb.Endpoint do
     signing_salt: "Q3dlFHYr"
   ]
 
-  socket "/socket", SponsorsWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :sponsors,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
