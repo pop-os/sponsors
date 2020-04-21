ffdc977a2198bd301369e92c807711752c719d74
Sean Callan <seancallan@gmail.com>
2020-04-21 08:13:26 -0600

    Add Credo

--- mix.exs
+++ mix.exs
@@ -33,13 +33,14 @@ defmodule Sponsors.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.16"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:phoenix, "~> 1.4.16"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:credo, "~> 1.4", only: [:dev, :test]}
     ]
   end
 
