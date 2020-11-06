6f694bbf726b8467a542a1a371f0bf6171d1b1ae
Blake Kostner <btkostner@gmail.com>
2020-11-06 12:05:07 -0700

    CD Deploy (#18)

--- config/releases.exs
+++ config/releases.exs
@@ -1,57 +1,32 @@
 import Config
 
-config :sponsors, SponsorsWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
-
-database_config =
-  "DB_SECRETS"
+config =
+  "CONFIG"
   |> System.fetch_env!()
   |> Jason.decode!()
+
+config :sponsors, SponsorsWeb.Endpoint, secret_key_base: config["SECRET_KEY_BASE"]
 
 config :sponsors, Sponsors.Repo,
-  database: database_config["dbname"],
-  username: database_config["username"],
-  password: database_config["password"],
-  hostname: database_config["host"]
+  database: config["DB_NAME"],
+  username: config["DB_USERNAME"],
+  password: config["DB_PASSWORD"],
+  hostname: config["DB_HOST"]
 
-config :sponsors, Sponsors.Mailer, api_key: System.fetch_env!("MAILGUN_API_KEY")
-
-stripe_config =
-  "STRIPE_SECRETS"
-  |> System.fetch_env!()
-  |> Jason.decode!()
+config :sponsors, Sponsors.Mailer, api_key: config["MAILGUN_KEY"]
 
 config :sponsors,
-  stripe_signing_secret: stripe_config["webhook_secret"],
-  subscription_plan: stripe_config["subscription_plan"]
+  stripe_signing_secret: config["STRIPE_WEBHOOK_SECRET"],
+  subscription_plan: config["STRIPE_SUBSCRIPTION_PLAN"]
 
-config :stripity_stripe, api_key: stripe_config["secret_key"]
+config :stripity_stripe, api_key: config["STRIPE_SECRET_KEY"]
 
-config :sponsors, Sponsors.Guardian, secret_key: System.fetch_env!("JWT_SECRET_KEY")
-
-# Since we do not include AppSignal reporting in all ENVs we need to handle their
-# configuration slightly differently.
-appsignal_defaults = %{
-  "active" => false,
-  "push_api_key" => "",
-  "env" => "prod"
-}
-
-appsignal_secrets =
-  "APPSIGNAL_SECRETS"
-  |> System.get_env("{}")
-  |> Jason.decode!()
-
-appsignal_config = Map.merge(appsignal_defaults, appsignal_secrets)
+config :sponsors, Sponsors.Guardian, secret_key: config["JWT_SECRET_KEY"]
 
 config :appsignal, :config,
-  active: appsignal_config["active"],
-  push_api_key: appsignal_config["push_api_key"],
-  env: appsignal_config["env"]
-
-slack_secrets =
-  "SLACK_SECRETS"
-  |> System.get_env("{}")
-  |> Jason.decode!()
+  active: Map.get(config, "APPSIGNAL_ENABLED", false),
+  push_api_key: config["APPSIGNAL_PUSH_KEY"],
+  env: config["APPSIGNAL_ENV"]
 
 config :sponsors,
-  slack_webhook: Map.get(slack_secrets, "subscriptions_webhook_url", nil)
+  slack_webhook: Map.get(config, "SLACK_WEBHOOK", nil)
