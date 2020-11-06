61fc340a8073114fdade8f5bcb88c1f738ba97ab
Blake Kostner <git@btkostner.io>
2020-11-05 22:23:07 -0700

    update configuration release file

--- config/releases.exs
+++ config/releases.exs
@@ -8,10 +8,10 @@ config =
 config :sponsors, SponsorsWeb.Endpoint, secret_key_base: config["SECRET_KEY_BASE"]
 
 config :sponsors, Sponsors.Repo,
-  database: database_config["DB_NAME"],
-  username: database_config["DB_USERNAME"],
-  password: database_config["DB_PASSWORD"],
-  hostname: database_config["DB_HOST"]
+  database: config["DB_NAME"],
+  username: config["DB_USERNAME"],
+  password: config["DB_PASSWORD"],
+  hostname: config["DB_HOST"]
 
 config :sponsors, Sponsors.Mailer, api_key: config["MAILGUN_KEY"]
 
@@ -22,21 +22,6 @@ config :sponsors,
 config :stripity_stripe, api_key: config["STRIPE_SECRET_KEY"]
 
 config :sponsors, Sponsors.Guardian, secret_key: config["JWT_SECRET_KEY"]
-
-# Since we do not include AppSignal reporting in all ENVs we need to handle their
-# configuration slightly differently.
-appsignal_defaults = %{
-  "active" => false,
-  "push_api_key" => "",
-  "env" => "prod"
-}
-
-appsignal_secrets =
-  "APPSIGNAL_SECRETS"
-  |> System.get_env("{}")
-  |> Jason.decode!()
-
-appsignal_config = Map.merge(appsignal_defaults, appsignal_secrets)
 
 config :appsignal, :config,
   active: Map.get(config, "APPSIGNAL_ENABLED", false),
