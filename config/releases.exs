52d60abe71f9cc8782dcb4c84df876cf2cd9f43c
Yongjin Chong <zephyranthes03@gmail.com>
2025-02-19 10:45:41 -0700

    Update PostgreSQL setting - SSL: true

--- config/releases.exs
+++ config/releases.exs
@@ -12,7 +12,10 @@ config :sponsors, Sponsors.Repo,
   username: config["DB_USERNAME"],
   password: config["DB_PASSWORD"],
   hostname: config["DB_HOST"],
-  ssl: true
+  ssl: [
+    verify: :verify_peer,
+    cacertfile: "/etc/ssl/certs/rds-ca-bundle.pem"
+  ]
 
 config :sponsors, Sponsors.Mailer, api_key: config["MAILGUN_KEY"]
 
